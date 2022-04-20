b2b34f5c5dc2d9c4c969648da6d9a1f2145298a5
ihossen016 <ihossen016@gmail.com>
2022-04-20 11:14:12 +0600

    bottom navigation create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import SimpleBottomNavigation from "./components/MainNav";
 
 function App() {
   return (
     <>
       <Header />
       <div className="App">sdasd</div>
+      <SimpleBottomNavigation />
     </>
   );
 }
