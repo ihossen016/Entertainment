4d866a1098aba025f42ddc14012b9f9ba5ad7aaa
ihossen016 <ihossen016@gmail.com>
2022-04-20 11:28:24 +0600

    header component moved to header folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/MainNav";
+import Container from "@material-ui/core/Container";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div className="App">sdasd</div>
+      <div className="App">
+        <Container>
+          <Switch>
+            <Route exact path="/" component={Trending} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
       <SimpleBottomNavigation />
-    </>
+    </BrowserRouter>
   );
 }
 
