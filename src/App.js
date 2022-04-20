d63f9021e5c3448b8e75284818cc2c6d9f7a7bf4
ihossen016 <ihossen016@gmail.com>
2022-04-20 10:57:59 +0600

    basic styles added to app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <div className="App">sdasd</div>
+    </>
   );
 }
 
