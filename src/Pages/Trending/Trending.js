ca6c63252c0f3f02d65a6ca242f56bc0e62b0476
ihossen016 <ihossen016@gmail.com>
2022-04-21 11:22:32 +0600

    trending page created

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
+
+// Styles
+import "./Trending.css";
 
 const Trending = () => {
   const [content, setContent] = useState([]);
@@ -9,6 +13,7 @@ const Trending = () => {
       `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
     );
 
+    // console.log(data.results);
     setContent(data.results);
   };
 
@@ -20,7 +25,18 @@ const Trending = () => {
     <div>
       <span className="pageTitle">Trending</span>
       <div className="trending">
-        {content && content.map(c => console.log(c))}
+        {content &&
+          content.map(c => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+            />
+          ))}
       </div>
     </div>
   );
