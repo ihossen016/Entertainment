ecd3b751e332f162cc69e21ee68e7b5403fbc77c
ihossen016 <ihossen016@gmail.com>
2022-04-22 16:54:43 +0600

    modal created

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,5 +1,6 @@
 import { Badge } from "@material-ui/core";
 import { img_300, unavailable } from "../../config/config";
+import ContentModal from "../ContentModal/ContentModal";
 
 // Styles
 import "./SingleContent.css";
@@ -13,7 +14,7 @@ const SingleContent = ({
   vote_average,
 }) => {
   return (
-    <div className="media">
+    <ContentModal media_type={media_type} id={id}>
       <Badge
         badgeContent={vote_average}
         color={vote_average > 6 ? "primary" : "secondary"}
@@ -29,7 +30,7 @@ const SingleContent = ({
         {media_type === "tv" ? "TV Series" : "Movie"}
         <span className="subTitle">{date}</span>
       </span>
-    </div>
+    </ContentModal>
   );
 };
 
